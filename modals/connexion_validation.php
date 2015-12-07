0b8119734994e46db11bcc0892ae2e70f886aac5
Skelotox <skelotox@hotmail.fr>
2015-12-07 16:28:08 +0100

    connexion, navbar

--- modals/connexion_validation.php
+++ modals/connexion_validation.php
@@ -3,26 +3,35 @@ session_start();
 include "../lib/db.php";
 
 if((isset($_POST["login"]) && !empty($_POST["login"])) && (isset($_POST["password"]) && !empty($_POST["password"]))){
-
+	/*var_dump("lol");
+	exit;*/
 	$select = $db->prepare("SELECT * FROM etudiant WHERE login = ? AND mdp= ? ");
-	$select->execute(array($_POST["login"], md5($_POST["password"]))) or die(print_r($select->errorInfo()));
+	$select->execute(array($_POST["login"], $_POST["password"])) or die(print_r($select->errorInfo()));
 	$nbRow = $select->rowCount();
 
 	if($nbRow == 0){
+		
 		$select = $db->prepare("SELECT * FROM enseignant WHERE login = ? AND mdp= ? ");
-		$select->execute(array($_POST["login"], md5($_POST["password"]))) or die(print_r($select->errorInfo()));
+		$select->execute(array($_POST["login"], $_POST["password"])) or die(print_r($select->errorInfo()));
 
 		$row = $select->fetch();
+		/*var_dump($row["login"]);
+		exit;*/
 		$_SESSION["login"]= $row["login"];
 		/*var_dump($row["nom"]);
 		exit;*/
+		$_SESSION["nom"]= $row["nom"];
 		$_SESSION["id_enseignant"]= $row["id_enseignant"];
 		$_SESSION["prenom"]= $row["prenom"];
 		$_SESSION["type"] = "enseignant";
+		/*var_dump($_SESSION["nom"]);
+		exit;*/
 		
 
 	}
 	else{
+		/*var_dump("lol3");
+		exit;*/
 		$row = $select->fetch();
 		$_SESSION["nom"]= $_POST["nom"];
         $_SESSION["login"] = $_POST["login"];
@@ -35,12 +44,12 @@ if((isset($_POST["login"]) && !empty($_POST["login"])) && (isset($_POST["passwor
 
 
 
-    $_SESSION["login"] = $_POST["login"];
+    /*$_SESSION["login"] = $_POST["login"];
     $_SESSION["id_etu"]= $_POST["id_etu"];
     $_SESSION["prenom"]= $_POST["prenom"];
-    $_SESSION["type"]= $_POST["type"];
+    $_SESSION["type"]= $_POST["type"];*/
 
 	$select->closeCursor();
-	header('Location: ../accueil.php');
+	header('Location: ../index.php');
 }
 ?>
--- partials/navbar.php
+++ partials/navbar.php
@@ -1,4 +1,6 @@
-
+<?php
+session_start();
+?>
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
@@ -8,21 +10,9 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
+            <a class="navbar-brand" href="index.php">Accueil</a>
 
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav navbar-left" id="left-nav">
-                <a class="navbar-brand" href="accueil.php">Accueil</a>
-                <?php
-                if(isset($_SESSION["login"]))
-                {?>
-                    <li><a href="liste_stage.php">Liste des Stages</a></li>
-                    <li><a href="planning_soutenance.php">Planning Soutenances</a></li>
-                    <li><a href="note.php">Notes</a></li>
-                <?php
-                }
-                ?>
-                </ul>
-            </div>
+            
 
 
 
@@ -31,17 +21,11 @@
 
             <ul class="nav navbar-nav navbar-right" id="right-nav">
                 <?php
-                $_SESSION["nom"]= "";
-                $_SESSION["prenom"]="";
-                $_SESSION["type"] = "";
-
-
-
-
                 if($_SESSION["nom"] && $_SESSION["prenom"]) {
 
                     if($_SESSION["type"] == "enseignant"){
                         ?>
+                        <li><a href="attribuer_etu.php">Assigner étudiant</a></li>
                         <li><a href="new_annonce.php">Poster annonce</a></li>
                         <li><a href="#"><?php echo $_SESSION["nom"].' '.$_SESSION["prenom"] ?></a></li>
                         <li><a href="#">Deconnexion</a></li>
@@ -49,35 +33,17 @@
                     }
                     else if ($_SESSION["type"] == "etudiant"){
                         ?>
-
+                        
                         <li><a href="#"><?php echo $_SESSION["nom"].' '.$_SESSION["prenom"] ?></a></li>
                         <li><a href="modals/deco_modal.php">Deconnexion</a></li>
                         <?php
                     }
-                }
-
-                ?>
-                <?php
-
-
-                if(!isset($_SESSION["login"]))
-                {
-                ?>
+                } else {
+                    ?>
                     <li><a href="" data-toggle="modal" data-target="#connexion" id="navConnexion">Connexion</a></li>
-                    <li><a href="" data-toggle="modal" data-target="#inscription" id="navInscription">Inscription</a></li>
-                <?php
-                }
-                else
-                {
-                ?>
-                <li><a><span class="alert-info">Bienvenue <?php echo $_SESSION["login"]; ?></span></a></li>
-                <li><a href="modals/deco_modal.php">Deconnexion</a></li>
-                <?php
+                    <?php
                 }
                 ?>
-
-
-
             </ul>
         </div>
     </div>
